1bd545352612fa086619bd230c03aed124994c14
Liu Chang <cliu@hkclr.hk>
2023-05-25 14:52:19 +0800

    complete resnet module

--- models/backbones/resnet.py
+++ models/backbones/resnet.py
@@ -11,7 +11,21 @@ from typing import Union, Optional
 # _resnet_type = cfg.RESNET.TYPE
 
 
-def _get_trans_block(trans_block_name: str) -> Union["ResBasicBlock", "ResBottleneckBlock"]:
+def _get_trans_block(
+        trans_block_name: str
+    ) -> Union["ResBasicBlock", "ResBottleneckBlock"]:
+    """Return ResNet Block class based on the string name provided by
+    `trans_blcok_name`.
+
+    Args:
+        trans_block_name (str):
+            The name of transformation block, should be one of 
+            `res_basic_block` or `res_bottleneck_block`.
+    Returns:
+        ResBlockClass (Union["ResBasicBlock", "ResBottleneckBlock"]):
+            The Transformation Block class that will be used in 
+            ResNet construction.
+    """
     _d = {
         "res_basic_block": ResBasicBlock,
         "res_bottleneck_block": ResBottleneckBlock
@@ -21,6 +35,7 @@ def _get_trans_block(trans_block_name: str) -> Union["ResBasicBlock", "ResBottle
 
 class ResBasicBlock(nn.Module):
     """The basic transformation block in ResNet-18/34.
+    Composition: 3x3, BN, AF, 3x3, BN.
     """
     
     def __init__(
@@ -30,21 +45,48 @@ class ResBasicBlock(nn.Module):
             c_out: int,
             c_b: int = None
         ) -> None:
+        """
+        Args:
+            stride (int):
+                Number of strides, stride=1 will maintain the feature
+                size unchanged, stride=2 will halve the feature height 
+                and width and double the channel number.
+            c_in (int):
+                Number of input channel.
+            c_out (int):
+                Number of output channel.
+            c_b (int):
+                Number of bottleneck channel, not used in this class. 
+                It's here for the purpose of api consistency.
+        
+        Returns:
+            None
+        """
         super(ResBasicBlock, self).__init__()
-        self.a_cnn = nn.Conv2d(c_in, c_out, 3, stride, 1)
-        self.a_bn = nn.BatchNorm2d(c_out)
-        self.a_af = nn.ReLU()
-        self.b_cnn = nn.Conv2d(c_out, c_out, 3, 1, 1)
-        self.b_bn = nn.BatchNorm2d(c_out)
+        self.l1_1 = nn.Conv2d(c_in, c_out, 3, stride, 1)
+        self.l1_2 = nn.BatchNorm2d(c_out)
+        self.l1_3 = nn.ReLU()
+        self.l2_1 = nn.Conv2d(c_out, c_out, 3, 1, 1)
+        self.l2_2 = nn.BatchNorm2d(c_out)
 
     def forward(self, X: torch.Tensor) -> torch.Tensor:
+        """Forward propogation.
+
+        Args:
+            X (Tensor):
+                Input tensor.
+        
+        Returns:
+            None
+        """
         for layer in self.children():
             X = layer(X)
         return X
 
 
 class ResBottleneckBlock(nn.Module):
-    """The bottleneck transformation block in ResNet-50/101/152: 
+    """The bottleneck transformation block in ResNet-50/101/152.
+    Composition: 1x1, BN, AF, 3x3, BN, AF, 1x1, BN.
     """
 
     def __init__(
@@ -54,17 +96,42 @@ class ResBottleneckBlock(nn.Module):
             c_out: int,
             c_b: int
         ) -> None:
+        """
+        Args:
+            stride (int):
+                Number of strides, stride=1 will maintain the feature
+                size unchanged, stride=2 will halve the feature height 
+                and width and double the channel number.
+            c_in (int):
+                Number of input channel.
+            c_out (int):
+                Number of output channel.
+            c_b (int):
+                Number of bottleneck channel.
+        
+        Returns:
+            None.
+        """
         super(ResBottleneckBlock, self).__init__()
-        self.a_cnn = nn.Conv2d(c_in, c_b, 1, 1, 0)
-        self.a_bn = nn.BatchNorm2d(c_b)
-        self.a_af = nn.ReLU()
-        self.b_cnn = nn.Conv2d(c_b, c_b, 3, stride, 1)
-        self.b_bn = nn.BatchNorm2d(c_b)
-        self.b_af = nn.ReLU()
-        self.c_cnn = nn.Conv2d(c_b, c_out, 1, 1, 0)
-        self.c_bn = nn.BatchNorm2d(c_out)
+        self.l1_1 = nn.Conv2d(c_in, c_b, 1, 1, 0)
+        self.l1_2 = nn.BatchNorm2d(c_b)
+        self.l1_3 = nn.ReLU()
+        self.L2_1 = nn.Conv2d(c_b, c_b, 3, stride, 1)
+        self.L2_2 = nn.BatchNorm2d(c_b)
+        self.L2_3 = nn.ReLU()
+        self.l3_1 = nn.Conv2d(c_b, c_out, 1, 1, 0)
+        self.l3_2 = nn.BatchNorm2d(c_out)
     
     def forward(self, X: torch.Tensor) -> torch.Tensor:
+        """Forward propogation.
+
+        Args:
+            X (Tensor):
+                Input tensor.
+        
+        Returns:
+            None
+        """
         for layer in self.children():
             X = layer(X)
         return X
@@ -72,6 +139,8 @@ class ResBottleneckBlock(nn.Module):
 
 class ResResidualBlock(nn.Module):
     """The residual block used in all ResNet series.
+    Composition: F(x) + x, where F(x) is one of `ResBasicBlock`
+    and `ResBottleneckBlock`, and x is the shortcut connection.
     """
 
     def __init__(
@@ -82,42 +151,182 @@ class ResResidualBlock(nn.Module):
             c_b: int,
             trans_block_name: str
         ) -> None:
+        """
+        Args:
+            stride (int):
+                Number of strides, stride=1 will maintain the feature
+                size unchanged, stride=2 will halve the feature height 
+                and width and double the channel number.
+            c_in (int):
+                Number of input channel.
+            c_out (int):
+                Number of output channel.
+            c_b (int):
+                Number of bottleneck channel. If `trans_block_name==
+                res_basic_block`, this arg will not be used.
+            trans_block_name (str):
+                Name of the transformation block, should be one of
+                `res_basic_block` or `res_bottleneck_block`.
+        
+        Returns:
+            None
+        """
         super(ResResidualBlock, self).__init__()
-        self.res_cnn = None
-        self.res_bn = None
-        self.af = nn.ReLU()
+        trans_block_cls = _get_trans_block(trans_block_name)
+        trans_block = trans_block_cls(stride, c_in, c_out, c_b)
         if c_in != c_out or stride > 1:
-            self.res_cnn = nn.Conv2d(c_in, c_out, 1, stride, 0)
-            self.res_bn = nn.BatchNorm2d(c_out)
-        trans_block = _get_trans_block(trans_block_name)
-        self.trans_block = trans_block(stride, c_in, c_out, c_b)
+            shortcut = nn.Sequential(
+                nn.Conv2d(c_in, c_out, 1, stride, 0),
+                nn.BatchNorm2d(c_out)
+            )
+        else:
+            shortcut = None
+        self.l1_1 = nn.ModuleDict({
+            "shortcut": shortcut,
+            "trans_block": trans_block
+        })
+        self.l1_2 = nn.ReLU()
     
     def forward(self, X: torch.Tensor) -> torch.Tensor:
-        X_ = self.trans_block(X)
-        if self.res_cnn is not None:
-            X_ += self.res_bn(self.res_cnn(X))
-        X_ = self.af(X_)
+        """Forward propogation.
+
+        Args:
+            X (Tensor):
+                Input tensor.
+        
+        Returns:
+            None
+        """
+        X_ = self.l1_1["trans_block"](X)
+        if self.l1_1["shortcut"]:
+            X_ += self.self.l1_1["shortcut"](X)
+        X_ = self.l1_2(X_)
         return X_
 
 
 class ResStage(nn.Module):
-    """Stage of ResNet
+    """The stage of ResNet. 
+    Composition: `d` numbers of `ResResidualBlock`.
     """
 
     def __init__(
             self, 
+            stride: int,
             c_in: int, 
             c_out: int, 
-            stride: int,
             d: int,
+            trans_block_name: str,
             c_b: Optional[int] = None,
         ):
-        super(ResStage, self).__init__()
-        for i in range(d):
-            if i
+        """
+        Args:
+            stride (int):
+                Number of strides, stride=1 will maintain the feature
+                size unchanged, stride=2 will halve the feature height 
+                and width and double the channel number.
+            c_in (int):
+                Number of input channel.
+            c_out (int):
+                Number of output channel.
+            d (int):
+                Depth, or the number of blocks, in this stage.
+            trans_block_name (str):
+                Name of the transformation block, should be one of
+                `res_basic_block` or `res_bottleneck_block`.
+            c_b (int):
+                Number of bottleneck channel. If `trans_block_name==
+                res_basic_block`, this arg will not be used. Default
+                None.
         
+        Returns:
+            None
+        """
+        super(ResStage, self).__init__()
+        res_blocks = []
+        for i in range(d):
+            if i == 0:
+                res_block = ResResidualBlock(stride, c_in, c_out, 
+                    c_b, trans_block_name)
+            else:
+                res_block = ResResidualBlock(1, c_out, c_out, c_b,
+                    trans_block_name)
+            res_blocks.append(res_block)
+        self.res_blocks = nn.ModuleList(res_blocks)
+                
+                
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        """Forward propogation.
+
+        Args:
+            X (Tensor):
+                Input tensor.
+        
+        Returns:
+            None
+        """
+        for res_block in self.res_blocks:
+            X = res_block(X)
+        return X
+    
+
+class ResStem(nn.Module):
+    """The stem used in all ResNet series.
+    Composition: 7x7, BN, Relu, MaxPool.
+    """
+    def __init__(self):
+        """
+        Args:
+            None
+        Returns:
+            None
+        """
+        super(ResStem, self).__init__()
+        self.l1 = nn.Sequential(
+            nn.Conv2d(3, 64, 7, 2, 3),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(3, 2, 1)
+        )
+
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        """Forward propogation.
+
+        Args:
+            X (Tensor):
+                Input tensor.
+        
+        Returns:
+            None
+        """
+        X = self.l1(X)
+        return X
+    
+
+class ResHead(nn.Module):
+    """The head used in all ResNet series.
+    Composition: AdaptiveAvgPool, Linear.
+    """
+    def __init__(self, c_in: int, num_classes: int):
+        super(ResHead, self).__init__()
+        self.l1_1 = nn.AdaptiveAvgPool2d((1, 1))
+        self.l1_2 = nn.Linear(c_in, num_classes)
+
+
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        """Forward propogation.
+
+        Args:
+            X (Tensor):
+                Input tensor.
+        
+        Returns:
+            None
+        """
+        X = self.l1_1(X)
+        X = self.l1_2(X.contiguous().view(X.shape[0], -1))
+        X = self.l1_2(X)
+        return X
 
 
 if __name__ == "__main__":
-    import sys
-    print(sys.path)
+    pass
