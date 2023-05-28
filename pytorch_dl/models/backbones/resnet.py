4051344915782c0075268eb6a4873b316d96b47d
liuchang <liu67511717@126.com>
2023-05-28 10:43:11 +0800

    ypdate resnet

--- pytorch_dl/models/backbones/resnet.py
+++ pytorch_dl/models/backbones/resnet.py
@@ -7,14 +7,21 @@
 
 import torch
 import torch.nn as nn
-from typing import Union, Optional, List
+from pytorch_dl.core.logging import get_logger
+from typing import Union, Optional, List, Dict, Any
 
+
+_VALID_TRANS_BLOCK_NAMES = ["res_basic_block", "res_bottleneck_block"]
+_VALID_HEAD_NAMES = ["res_linear_head", "res_conv_head", "res_light_head"]
+
+
+_logger = get_logger(__name__)
 
 
 def _get_trans_block(
         trans_block_name: str
     ) -> Union["ResBasicBlock", "ResBottleneckBlock"]:
-    """Return ResNet Block class based on the string name provided by
+    """Return ResNet block class based on the string name provided by
     `trans_blcok_name`.
 
     Args:
@@ -23,7 +30,7 @@ def _get_trans_block(
             `res_basic_block` or `res_bottleneck_block`.
     Returns:
         ResBlockClass (Union["ResBasicBlock", "ResBottleneckBlock"]):
-            The Transformation Block class that will be used in 
+            The transformation block class that will be used in 
             ResNet construction.
     """
     _d = {
@@ -31,6 +38,29 @@ def _get_trans_block(
         "res_bottleneck_block": ResBottleneckBlock
     }
     return _d[trans_block_name]
+
+
+def _get_head(
+        head_name: str
+    ) -> Union["ResLinearHead", "ResConvHead", "ResLightHead"]:
+    """Return ResNet head class based on the string name provided by
+    head_name.
+
+    Args:
+        head_name (str):
+            The name of head, should be one of `res_linear_head`, 
+            `res_conv_head`, `res_light_head`.
+    
+    Returns:
+        ResHead (Union["ResLinearHead", "ResConvHead", "ResLightHead"]):
+            The ResNet head class.
+    """
+    _d = {
+        "res_linear_head": ResLinearHead,
+        "res_conv_head": ResConvHead,
+        "res_light_head": ResLightHead
+    }
+    return _d[head_name]
 
 
 def _init_weight(m: nn.Module) -> None:
@@ -442,31 +472,154 @@ class ResNet(nn.Module):
             self, 
             num_classes: int,
             stage_depths: List[int],
+            stage_strides: List[int],
             stage_widths: List[int],
-            stage_bottleneck_widths: List[Optional[int]],
+            stage_bottleneck_widths: Optional[List[Optional[int]]],
             trans_block_name: str,
             head_name: str
         ) -> None:
-        """Args:
-            stage_depths (List[int]):
-                The depth of each stage.
+        f"""Args:
             num_classes (int):
                 The number of classes.
+            stage_depths (List[int]):
+                The depth of each stage.
+            stage_strides (List[int]):
+                The stride of each stage.
             stage_widths (List[int]):
                 The number of output channel of each stage.
-            stage_bottleneck_widths (List[int]):
+            stage_bottleneck_widths (Optional[List[Optional[int]]]):
                 The number of bottleneck channels.
             trans_block_name (str):
-                The name of transformation block in ResStage, should be either
-                `res_basic_block` or `res_bottleneck_block`.
+                The name of transformation block in ResStage, should be one of
+                {_VALID_TRANS_BLOCK_NAMES}.
+            head_name (str):
+                The name of head, should be one of {_VALID_HEAD_NAMES}.
+        
+        Returns:
+            None.
         """
         super(ResNet, self).__init__()
-        assert len(stage_depths) == 0 and len(stage_widths), \
-            "stage_depths and stage_widths should have 4 elements."
-        stage_strides = [1, 2, 2, 2]
+        param_dict = {
+            "num_classes": num_classes,
+            "stage_depths": stage_depths,
+            "stage_widths": stage_widths,
+            "stage_strides": stage_strides,
+            "stage_bottleneck_widths": stage_bottleneck_widths,
+            "trans_block_name": trans_block_name,
+            "head_name": head_name
+        }
+        self._param_check(**param_dict)
+        self._construct_resnet(**param_dict)
+    
+    
+    def _param_check(
+            self, 
+            num_classes: int,
+            stage_depths: List[int],
+            stage_strides: List[int],
+            stage_widths: List[int],
+            stage_bottleneck_widths: Optional[List[Optional[int]]],
+            trans_block_name: str,
+            head_name: str
+        ) -> None:
+        f"""Perform the following parameter check:
+            * If 'trans_block_name' is one of {_VALID_TRANS_BLOCK_NAMES}.
+            * If 'head_name' is one of {_VALID_HEAD_NAMES}.
+            * If 'stage_depths', 'stage_strides', 'stage_widths' have equal
+            lengths for 'res_basic_block'.
+            * If 'stage_depths', 'stage_strides', 'stage_widths', 
+            'stage_bottleneck_widths' have equal lengths for
+            'res_bottleneck_block'.
+            * For 'res_light_head', if `stage_widths[-1] != num_classes`,
+            raise a warning and reassign `stage_widths[-1] = num_classes`.
+        
+        Args:
+            num_classes (int):
+                The number of classes.
+            stage_depths (List[int]):
+                The depth of each stage.
+            stage_strides (List[int]):
+                The stride of each stage.
+            stage_widths (List[int]):
+                The number of output channel of each stage.
+            stage_bottleneck_widths (Optional[List[Optional[int]]]):
+                The number of bottleneck channels.
+            trans_block_name (str):
+                The name of transformation block in ResStage, should be one of
+                {_VALID_TRANS_BLOCK_NAMES}.
+            head_name (str):
+                The name of head, should be one of {_VALID_HEAD_NAMES}.
+        
+        Returns:
+            None.
+        """
+        assert trans_block_name in _VALID_TRANS_BLOCK_NAMES, \
+            "'trans_block_name={0}' is not a valid block. Valid blocks \
+            are {1}".format(trans_block_name, _VALID_TRANS_BLOCK_NAMES)
+        assert head_name in _VALID_HEAD_NAMES, \
+            "'head_name={0}' is not a valid head. Valid heads \
+            are {1}".format(head_name, _VALID_HEAD_NAMES)
+        if trans_block_name == "res_basic_block":
+            assert len(stage_depths) == len(stage_strides) \
+                == len(stage_widths), "'stage_depths', \
+                'stage_strides', 'stage_widths' should \
+                have equal lengths."
+            stage_bottleneck_widths = [None for i in range(len(stage_depths))]
+        elif trans_block_name == "res_bottleneck_block":
+            assert len(stage_depths) == len(stage_strides) \
+                == len(stage_widths) == len(stage_bottleneck_widths), \
+                "'stage_depths', 'stage_strides', 'stage_widths', \
+                'stage_bottleneck_widths' should have equal lengths."
+        else:
+            pass
+        
+        if head_name == "res_light_head":
+            if stage_widths[-1] != num_classes:
+                _logger.warn(
+                    "You are using 'res_light_head', but your 'stage_widths[-1]' \
+                    is not equal to 'num_class'. The program will reassign it to \
+                    'num_classes', but you should be conscious of this reassignment  \
+                    and ensure it follows your willness."
+                )
+                stage_widths[-1] = num_classes
+
+    
+    def _construct_resnet(
+            self,
+            num_classes: int,
+            stage_depths: List[int],
+            stage_strides: List[int],
+            stage_widths: List[int],
+            stage_bottleneck_widths: Optional[List[Optional[int]]],
+            trans_block_name: str,
+            head_name: str
+        ) -> None:
+        """Build ResNet.
+        
+        Args:
+            num_classes (int):
+                The number of classes.
+            stage_depths (List[int]):
+                The depth of each stage.
+            stage_strides (List[int]):
+                The stride of each stage.
+            stage_widths (List[int]):
+                The number of output channel of each stage.
+            stage_bottleneck_widths (Optional[List[Optional[int]]]):
+                The number of bottleneck channels.
+            trans_block_name (str):
+                The name of transformation block in ResStage, should be one of
+                {_VALID_TRANS_BLOCK_NAMES}.
+            head_name (str):
+                The name of head, should be one of {_VALID_HEAD_NAMES}.
+        
+        Returns:
+            None.
+        """
         c_init = stage_widths[0]
         c_last = stage_widths[0]
         res_stages = []
+        head_cls = _get_head(head_name)
         for s, d, c, c_b in zip(stage_strides, 
                 stage_depths, stage_widths, stage_bottleneck_widths):
             res_stage = ResStage(s, c_last, c, d, trans_block_name, c_b)
@@ -474,10 +627,10 @@ class ResNet(nn.Module):
             c_last = c
         self.l0 = ResStem(c_init)
         self.l1 = nn.ModuleList(res_stages)
-        self.l2 = ResLinearHead(c_last, num_classes)
+        self.l2 = head_cls(c_last, num_classes)
         self.apply(_init_weight)
-    
 
+    
     def forward(self, X: torch.Tensor) -> torch.Tensor:
         """Forward propogation.
 
